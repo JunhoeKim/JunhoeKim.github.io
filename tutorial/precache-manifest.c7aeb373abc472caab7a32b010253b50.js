a5a2d6829ac3c89e2df5ff9ff8e5798fc6b96528
kev-damian <maheu@outlook.com>
2020-03-09 01:35:58 +0900

    tutorial alpha

---
+++ tutorial/precache-manifest.c7aeb373abc472caab7a32b010253b50.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da725e6f26b22597728601ff22decd4",
+    "url": "/tutorial/index.html"
+  },
+  {
+    "revision": "c763839449bca4ce2e53",
+    "url": "/tutorial/static/js/0.d45fdaeb.chunk.js"
+  },
+  {
+    "revision": "636c063e471f036df7a0",
+    "url": "/tutorial/static/js/3.b7f0acf6.chunk.js"
+  },
+  {
+    "revision": "349a03097e8eddedac8cc67f26a159d7",
+    "url": "/tutorial/static/js/3.b7f0acf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "636b10985a256b834629",
+    "url": "/tutorial/static/js/4.ec505971.chunk.js"
+  },
+  {
+    "revision": "a73ae50cc0b9f13c21dd696a093a7ee8",
+    "url": "/tutorial/static/js/4.ec505971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449cdea629dbc93e2b64",
+    "url": "/tutorial/static/js/5.a9a28eb0.chunk.js"
+  },
+  {
+    "revision": "7dc80164bf611a2d82bc",
+    "url": "/tutorial/static/js/6.17194b3b.chunk.js"
+  },
+  {
+    "revision": "8618b9c57d1d337f2b98",
+    "url": "/tutorial/static/js/7.9d22fd0e.chunk.js"
+  },
+  {
+    "revision": "542783c42b7bfb8e8628",
+    "url": "/tutorial/static/js/8.35d04921.chunk.js"
+  },
+  {
+    "revision": "fb27ccd0003133f3abb9",
+    "url": "/tutorial/static/js/main.b246ddf1.chunk.js"
+  },
+  {
+    "revision": "27128f8ef6a83b7ba5d3",
+    "url": "/tutorial/static/js/runtime-main.4596d74d.js"
+  },
+  {
+    "revision": "6747fb5f88bd5977143ec37eb6baf202",
+    "url": "/tutorial/static/media/annotation_interface.6747fb5f.png"
+  }
+]);
